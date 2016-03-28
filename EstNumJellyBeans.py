d6aa67306999b4af059bf218b4ba745843bc9c05
Brighton Skeel <bskeel@berkeley.edu>
2016-03-27 21:42:03 -0700

    Finished all the work here

--- EstNumJellyBeans.py
+++ EstNumJellyBeans.py
@@ -49,12 +49,12 @@ class NumJellyEstimator:
             "Error: variable 'people' must be an integer"
  
         # NE24: Add a test for value here
-        if(fracPplLovingPink == 0):
+        if(self.fracPplLovingPink == 0):
             if(people < 0):
                 print ("Error: variable 'people' cannot be a negative value")
                 #Checks to see if the input value of people is negative
         else:
-            if(people <= (1/fracPplLovingPink)):
+            if(people <= (1/self.fracPplLovingPink)):
                 print ("Error: People Must be a positive integer value")
                 #Accounts for the case where the fraction of people loving pink is nonzero (the fraction must ensure there is an integer number of people who like pink)
             
--- ExampleInput.py
+++ ExampleInput.py
@@ -9,8 +9,48 @@ estimator = jelly.NumJellyEstimator()
 # Set the fraction of land being used for sugar, the world population,
 # and the fraction of the population that loves pink.
 land_frac = 0.1
-ppl = 7e9
+ppl = 7000000000
 pink_frac = 0.18
+
+#Give some examples of test data
+
+#test the data type test for land_frac
+
+#land_frac = a
+#ppl = 7000000000
+#pink_frac = 0.18
+
+#test the range for land_frac
+
+#land_frac = -1
+#ppl = 7000000000
+#pink_frac = 0.18
+
+#test the data type test for ppl
+
+# land_frac = 0.1
+# ppl = a
+# pink_frac = 0.18
+
+#test the variale range for the ppl variable
+
+# land_frac = 0.1
+# ppl = -1
+# pink_frac = 0.18
+
+#test the data type for pink_frac
+
+# land_frac = 0.1
+# ppl = 7000000000
+# pink_frac = a
+
+#test the range for the variable pink_frac
+
+# land_frac = 0.1
+# ppl = 700000000
+# pink_frac = 1.1
+
+
 estimator.set_land_frac_for_sugar(land_frac)
 estimator.set_world_pop(ppl)
 estimator.set_frac_ppl_loving_pink(pink_frac)
